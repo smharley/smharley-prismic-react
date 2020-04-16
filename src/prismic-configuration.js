4981c73707be028207ebe83e1314daf1d45eacab
Steven Harley <smharley@gmail.com>
2020-04-15 19:19:47 -0700

    Remove index.js cruft, Add Prismic config

--- src/prismic-configuration.js
+++ src/prismic-configuration.js
@@ -1,6 +1,13 @@
-exports.linkResolver = function linkResolver(doc) {
-   if (doc.type === "post") {
-      return "/blog/" + doc.uid
-   }
-   return "/"
+import Prismic from 'prismic-javascript'
+
+export const accessToken = process.env.REACT_APP_API_TOKEN
+export const apiEndpoint = process.env.REACT_APP_API_URL
+export const client = Prismic.client(apiEndpoint, { accessToken })
+
+export const linkResolver = (doc) => {
+  if (doc.type === 'blog_post') {
+    return '/blog/' + doc.uid
+  }
+
+  return '/'
 }
